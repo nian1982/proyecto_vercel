84d65554c61a1c485a35acf398f8e1a3d9fcb7a9
Nelson Rodriguez <Nelson.Rodriguez8203@gmail.com>
2024-05-29 18:59:54 -0500

    configuraciones del servidor y plantillas finalizado, creacion navbar y configuracion de la plantilla funcional

--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 import express from "express"
+import morgan from 'morgan';
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
 
 const app = express()
 
-app.set("port", process.env.PORT || 4000)
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
+app.set("port", process.env.PORT || 4000)
+app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.use(morgan('dev'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.json({ "message": "Welcom to my messages API"})
-})
+    res.render('index')
+});
+
+app.use(express.static(join(__dirname, 'public')));
 
 export default app;
